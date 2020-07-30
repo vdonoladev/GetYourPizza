e3d845fbf015ee683d331e6b27526eba1d7bdd3c
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-07-30 18:45:37 -0300

    Adicionando as informações das pizzas na tela

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const cs = (el)=>document.querySelectorAll(el);
 
 pizzaJson.map(function(item, index) {
 	let pizzaItem = c('.models .pizza-item').cloneNode(true);
-	// preencher as informações em pizzaitem
+	
+	pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+	pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+	pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+	pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+	
 
 	c('.pizza-area').append( pizzaItem );
 });
