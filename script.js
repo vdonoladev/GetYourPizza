ec28bbd8e5190b5eb83f3225d8e0687435232c3e
Víctor Donola Ferreira <victordonolaf@gmail.com>
2020-07-31 17:30:44 -0300

    Começando a colocar as funções no 'modal'

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let modalQt = 1;
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
+// Listagem das pizzas
 pizzaJson.map(function(item, index) {
 	let pizzaItem = c('.models .pizza-item').cloneNode(true);
 	
@@ -11,6 +12,7 @@ pizzaJson.map(function(item, index) {
 	pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
 	pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
 	pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+	
 	pizzaItem.querySelector('a').addEventListener('click', (e)=>{
 		e.preventDefault();
 		let key = e.target.closest('.pizza-item').getAttribute('data-key');
@@ -38,4 +40,31 @@ pizzaJson.map(function(item, index) {
 	});
 
 	c('.pizza-area').append(pizzaItem);
+});
+
+// Eventos do MODAL
+function closeModal() {
+	c('.pizzaWindowArea').style.opacity = 0;
+	setTimeout(()=>{
+		c('.pizzaWindowArea').style.display = 'none';
+	}, 500);
+}
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+	item.addEventListener('click', closeModal);
+});
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+	if(modalQt > 1) {
+		modalQt--;
+		c('.pizzaInfo--qt').innerHTML = modalQt;
+	}
+});
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+	modalQt++;
+	c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+	size.addEventListener('click', (e)=>{
+		c('.pizzaInfo--size.selected').classList.remove('selected');
+		size.classList.add('selected');
+	});
 });
